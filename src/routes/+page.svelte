7b5bef1a42d3ef7474cbc36f936a98663ba853e3
Naveen Mathew <naveenmathew1585@gmail.com>
2024-06-26 18:22:50 +0530

    ✨ get data from pb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,17 @@
     import { Button } from "$lib/components/ui/button";
     import { Input } from "$lib/components/ui/input";
     import * as Card from "$lib/components/ui/card";
+    import { pb } from "$lib/pocketbase";
+    import { onMount } from "svelte";
+
+    let items = [];
+
+    onMount(async () => {
+        items = await pb.collection("items").getFullList({
+            sort: "-created",
+        });
+        console.log(items);
+    });
 </script>
 
 <div class="flex items-center justify-center max-w-md gap-2 mx-auto">
@@ -14,9 +25,9 @@
     <div class="flex items-center gap-2 text-xl font-semibold">
         <Icon icon="ph:floppy-disk-duotone" class="text-2xl text-primary" /> Saved Items
     </div>
-    {#each Array(5) as item, i}
+    {#each items as item}
         <Card.Root class="flex items-center justify-between px-3 py-2">
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit labore et {i}</div>
+            <div>{item.content}</div>
             <Button variant="ghost">
                 <Icon icon="ph:trash-duotone" class="text-lg text-rose-500" />
             </Button>
