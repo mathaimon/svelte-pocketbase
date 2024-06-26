e73ae40a1c6492ada2474ec77c80701ea5614e38
Naveen Mathew <naveenmathew1585@gmail.com>
2024-06-26 16:39:20 +0530

    🚨 fixed formating

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import autoImport from 'sveltekit-autoimport';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,18 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess:[
+		autoImport({
+			components: [
+				{ name: './src/components', flat: true },
+				{ name: './src/lib/components', flat: true }
+			 ],
+			mapping:{
+				Icon: `import Icon from '@iconify/svelte'`
+			}
+		}),
+	]
 };
 
 export default config;
